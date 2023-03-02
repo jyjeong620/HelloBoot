9c85a9da8b09830c4d20ffb9035beea974fba8c7
정준영 <jeongjy@mrblue.com>
2023-03-02 18:46:50 +0900

    feat: WebServer 띄우는방식

--- src/main/java/jeongjy/helloboot/HelloBootApplication.java
+++ src/main/java/jeongjy/helloboot/HelloBootApplication.java
@@ -1,13 +1,16 @@
 package jeongjy.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class HelloBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloBootApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
-
 }
+
+// 1. 톰켓도 자바로 만든 서버이다.
