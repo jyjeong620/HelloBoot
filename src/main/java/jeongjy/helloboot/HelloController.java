e641cdb84ad9d1841668eee020dcdedc645115e2
정준영 <jeongjy@mrblue.com>
2023-03-07 19:31:30 +0900

    feat: ComponentScan을 이용하여 빈등록 하도록 변경

--- src/main/java/jeongjy/helloboot/HelloController.java
+++ src/main/java/jeongjy/helloboot/HelloController.java
@@ -2,11 +2,12 @@ package jeongjy.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RequestMapping
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -15,7 +16,6 @@ public class HelloController {
     }
 
     @GetMapping("/hello")
-    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
