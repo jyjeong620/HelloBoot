7ff097d2985aeb8429225d2b8a0e289158e5dc39
정준영 <jeongjy@mrblue.com>
2023-03-09 19:02:02 +0900

    feat: api fail testcode add

--- src/test/java/jeongjy/helloboot/HelloApiTest.java
+++ src/test/java/jeongjy/helloboot/HelloApiTest.java
@@ -1,20 +1,18 @@
 package jeongjy.helloboot;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloApiTest {
 
     @Test
-    void HelloApi (){
+    void helloApi() {
         // http localhost:8080/hello?name=Spring
         TestRestTemplate rest = new TestRestTemplate();
 
@@ -27,6 +25,19 @@ public class HelloApiTest {
         assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
         // body Hello Spring
         assertThat(res.getBody()).isEqualTo("Hello Spring");
+
+    }
+
+    @Test
+    void failHelloApi() {
+        // http localhost:8080/hello?name=Spring
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res
+                = rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+
+        // Status code 500
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 }
