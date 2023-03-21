ec69aaa9b791411e14c98f24395fed1073ce527f
정준영 <jeongjy@mrblue.com>
2023-03-21 19:30:10 +0900

    feat: Meta annotation을 이용한 동적 Config 설정 방식 추가

--- src/main/java/jeongjy/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/jeongjy/config/autoconfig/JettyWebServerConfig.java
@@ -1,26 +1,16 @@
 package jeongjy.config.autoconfig;
 
+import jeongjy.config.ConditionalMyOnClass;
 import jeongjy.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean(name = "jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
-    }
-
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
-        }
     }
 }
