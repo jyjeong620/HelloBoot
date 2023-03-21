36cce1467fd6f088a55848eb9be2a83641ea955d
정준영 <jeongjy@mrblue.com>
2023-03-21 19:49:01 +0900

    feat: 사용자 구성정보에서 Custom 설정을 할경우 기본 설정 정보와 중복되는것을 방지하는 로직 추가

--- src/main/java/jeongjy/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/jeongjy/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package jeongjy.config.autoconfig;
 
 import jeongjy.config.ConditionalMyOnClass;
 import jeongjy.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean(name = "jettyWebServerFactory")
+    @ConditionalOnMissingBean       // Bean이 없을 때만 생성
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
--- src/main/java/jeongjy/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/jeongjy/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package jeongjy.config.autoconfig;
 
 import jeongjy.config.ConditionalMyOnClass;
 import jeongjy.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean(name = "tomcatWebServerFactory")
+    @ConditionalOnMissingBean       // Bean이 없을 때만 생성
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
