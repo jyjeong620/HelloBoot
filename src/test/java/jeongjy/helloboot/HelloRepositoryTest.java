7fc26d37b082047e039d3949078b6524672aabac
정준영 <jeongjy@mrblue.com>
2023-03-30 19:18:35 +0900

    feat: jdbcTemplate repository 생성 및 테스트

---
+++ src/test/java/jeongjy/helloboot/HelloRepositoryTest.java
@@ -0,0 +1,39 @@
+package jeongjy.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@HelloBootTest
+public class HelloRepositoryTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @Autowired
+    HelloRepository helloRepository;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(30) primary key, count int)");
+    }
+
+    @Test
+    void findHelloFailed() {
+        assertThat(helloRepository.findHello("jeongjy")).isNull();
+    }
+
+    @Test
+    void increaseCount() {
+        assertThat(helloRepository.countOf("jeongjy")).isEqualTo(0);
+
+        helloRepository.increaseCount("jeongjy");
+        assertThat(helloRepository.countOf("jeongjy")).isEqualTo(1);
+
+        helloRepository.increaseCount("jeongjy");
+        assertThat(helloRepository.countOf("jeongjy")).isEqualTo(2);
+    }
+}
