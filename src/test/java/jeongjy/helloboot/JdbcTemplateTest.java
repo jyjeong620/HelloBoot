c5ac89463cae6c8ea6f669180b5e653f38533811
정준영 <jeongjy@mrblue.com>
2023-03-30 19:01:54 +0900

    feat: jdbcTemplate connection 추가 및 테스트코드 추가

---
+++ src/test/java/jeongjy/helloboot/JdbcTemplateTest.java
@@ -0,0 +1,29 @@
+package jeongjy.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HelloBootTest
+public class JdbcTemplateTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(30) primary key, count int)");
+    }
+
+
+    @Test
+    void insertAndQuery() {
+        jdbcTemplate.update("insert into hello values (?, ?)", "jeongjy", 1);
+        jdbcTemplate.update("insert into hello values (?, ?)", "Spring", 2);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        Assertions.assertThat(count).isEqualTo(2);
+    }
+}
